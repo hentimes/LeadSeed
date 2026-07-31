853aa3d5bce51d88619d3e56ee9e86a77c8cc1e7
Henry Farias <planespro.cl@gmail.com>
2026-07-31 19:01:15 -0400

    chore: limpiar scripts ad-hoc, consultas temporales y auditorias ejecutadas

--- tmp_check_google.sql
+++
@@ -1,4 +0,0 @@
-select user_id, provider, google_email, token_scope, token_expires_at, (refresh_token is not null) as has_refresh, (access_token is not null) as has_access
-from public.user_calendar_connections
-where google_email in ('planespro.cl@gmail.com','hentimes@gmail.com')
-order by google_email;
--- tmp_check_users.sql
+++
@@ -1,3 +0,0 @@
-select p.id, p.email, p.email_provider
-from public.profiles p
-where p.email in ('planespro.cl@gmail.com','hentimes@gmail.com');
